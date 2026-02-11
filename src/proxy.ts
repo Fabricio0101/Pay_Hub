e4bdc5030ec5e7ffe2987c44edebfe1ff5c1d37e
ArcturoTech <arcturustech.enterprise@gmail.com>
2026-02-11 16:10:09 -0300

    feat: update Next version and change name middleware for proxy

--- src/proxy.ts
+++ src/proxy.ts
@@ -1,7 +1,7 @@
 import { NextResponse } from "next/server";
 import type { NextRequest } from "next/server";
 
-export function middleware(request: NextRequest) {
+export function proxy(request: NextRequest) {
   const { pathname } = request.nextUrl;
 
   // Se for a rota raiz, redirecionar para login
@@ -26,4 +26,3 @@ export const config = {
     "/((?!api|_next/static|_next/image|favicon.ico|.*\\.(?:svg|png|jpg|jpeg|gif|webp|mp4)$).*)",
   ],
 };
-
